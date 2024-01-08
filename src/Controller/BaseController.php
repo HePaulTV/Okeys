eff310e197c0a410e548827d1a0c1e8569820b84
Nathan <nathan.debuire@gmail.com>
2024-01-08 17:35:23 +0100

    first commit

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    #[Route('/', name: 'base')]
+    public function index(): Response
+    {
+        return $this->render('base/index.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('base/contact.html.twig', [
+            
+        ]);
+    }
+}
